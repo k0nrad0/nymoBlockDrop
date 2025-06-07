cee5f00a2cf4d9e3502af99b28dfcd4450d3e728
konrad0 <94558939+k0nrad0@users.noreply.github.com>
2025-06-07 18:53:41 +0200

    init commit

---
+++ src/main/java/com/konrad0/nymoBlockDrop/NymoBlockDrop.java
@@ -0,0 +1,17 @@
+package com.konrad0.nymoBlockDrop;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NymoBlockDrop extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("nymoBlockDrop enabled :D");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
